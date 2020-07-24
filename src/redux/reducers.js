498623c192cd418646e53b8b9de31cf21369ae53
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-07-24 23:50:55 +0500

    More changes to styles/reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-fallthrough */
 import * as types from './types';
 
-export const initialState = (localStorage.length > 0 || localStorage.library === '') ? [...JSON.parse(localStorage.library)] : [
+// part after && in parens below should be removed when the 3 builtin book items are removed
+export const initialState = (localStorage.length > 0 && localStorage.library !== '[]') ? [...JSON.parse(localStorage.library)] : [
   {id: 'c482f0f3-7a84-4c4d-bd60-feb7c9908351', title: 'Eloquent JavaScript',
     author: 'Marijn Haverbeke', pages: 448, read: true},
   {id: '71f6dc97-ae31-4140-a4f0-1068d180a018', title: 'CODE', 
     author: 'Charles Petzold', pages: 391, read: true},
   {id: 'ade67292-d0cd-4010-a6ac-25de9c4546fc', title: '30 Days of React',
     author: 'Avi Lerner', pages: 304, read: true}
-];
+]; // Emp
 
 const updateLocalStorage = library => {
   localStorage.clear();
