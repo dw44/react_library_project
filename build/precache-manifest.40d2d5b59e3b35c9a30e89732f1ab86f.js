eb323b52a69e362f2ccb432beff731c3ef9ef62b
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-05-13 21:22:13 -0400

    updated

---
+++ build/precache-manifest.40d2d5b59e3b35c9a30e89732f1ab86f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da585f90974b525f63217fef44dc0ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de49845ce8384d69524",
+    "url": "/static/css/main.9600ca58.chunk.css"
+  },
+  {
+    "revision": "86ae225afe39938efdbc",
+    "url": "/static/js/2.bb60f3cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb60f3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de49845ce8384d69524",
+    "url": "/static/js/main.a43a838c.chunk.js"
+  },
+  {
+    "revision": "d5820ea9925102de94ea",
+    "url": "/static/js/runtime-main.3c74cd86.js"
+  }
+]);
